6a7582f39ce106a92698fc30259e35528ce199b9
ZHAISHENKING <18700790825@163.com>
2020-04-22 15:56:34 +0800

    add addCom

---
+++ 01_腾讯云/千位分隔符.js
@@ -0,0 +1,20 @@
+function addComma(num) {
+  // TODO
+  let nums = num.toString();
+  let result = ''
+  let count = 1
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result = nums[i] + result
+    if (count % 3 === 0 && i !== 0) {
+      result = ',' + result
+    }
+    count++
+  }
+  return result
+}
+
+//
+console.log(addComma(123456789))
+
+// let a = 1000
+// console.log(a.toLocaleString()) // 1,000
